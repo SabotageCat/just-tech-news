15e97f7e5c0554db4318a6d7466235591b256521
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-05-03 15:56:13 -0500

    added comment feature for users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -23,9 +23,21 @@ router.get('/:id', (req, res) => {
         include: [
             {
                 model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
                 attributes: ['title'],
                 through: Vote,
                 as: 'voted_posts'
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                  model: Post,
+                  attributes: ['title']
+                }
             }
         ]
     })
